0353e954b805d2472155796c9c110a98ab609d3b
NetworkClassificationProject <isurualagiyawanna9717@gmail.com>
2020-04-20 18:23:45 +0530

    # rename the main script

--- variables.py
+++ variables.py
@@ -6,7 +6,7 @@ dense1 = 512
 dense2 = 512
 dense3 = 128
 dense4 = 128
-denset = 64
+denset = 32
 keep_prob = 0.3
 output_dim = 256
 n_features = 48
@@ -15,9 +15,9 @@ num_epoches = 100
 validation_split = 0.2
 
 #data paths and model weights
-big_csv = os.path.join(os.getcwd(), 'FGISY0.5s.csv')
-tl_path = os.path.join(os.getcwd(), 'M0.5s.csv') # tl - transfer_learning
-model_path =  os.path.join(os.getcwd(), 'base_model.json')
-model_weights =  os.path.join(os.getcwd(), 'base_model.h5')
-tl_model_path =  os.path.join(os.getcwd(), 'massenger.json')
-tl_model_weights =  os.path.join(os.getcwd(), 'massenger.h5')
+big_csv = os.path.join(os.getcwd(), 'DataFiles/FGISY0.5s.csv')
+tl_path = os.path.join(os.getcwd(), 'DataFiles/M0.5s.csv') # tl - transfer_learning
+model_path =  os.path.join(os.getcwd(), 'DataFiles/base_model.json')
+model_weights =  os.path.join(os.getcwd(), 'DataFiles/base_model.h5')
+tl_model_path =  os.path.join(os.getcwd(), 'DataFiles/massenger.json')
+tl_model_weights =  os.path.join(os.getcwd(), 'DataFiles/massenger.h5')
