d01fec0f255216783e7600bde60db9d517a1d765
1zuu <isurualagiyawanna9717@gmail.com>
2021-01-18 13:14:13 +0530

    CNN model

---
+++ CNN model/cnn.py
@@ -0,0 +1,179 @@
+import os
+import logging
+import operator
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from collections import Counter
+from matplotlib import pyplot as plt
+from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam, RMSprop
+from tensorflow.keras.models import Model, load_model
+from tensorflow.keras.layers import Dense, Input, Dropout, Conv2D, MaxPool2D, Flatten
+
+logging.getLogger('tensorflow').disabled = True
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+physical_devices = tf.config.experimental.list_physical_devices('GPU')
+print("\nNum GPUs Available : {}".format(len(physical_devices)))
+print("Tensorflow version : {}\n".format(tf.__version__))
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+from variables import*
+from util import load_data
+
+class TrafficClassifier(object):
+    def __init__(self):
+        X, Y, Xtest = load_data()
+        self.X = X 
+        self.Y = Y
+        self.Xtest = Xtest
+        print("Input Shape : {}".format(self.X.shape))
+        print("Label Shape : {}".format(self.Y.shape))
+
+    def classifier(self):
+        inputs = Input(shape=input_shape)
+        x = Conv2D(256, (3,3), activation='relu')(inputs)
+        x = MaxPool2D(pool_size=(2, 2), strides=(2,2))(x)
+        x = Conv2D(128, (3,3), activation='relu')(x)
+        x = MaxPool2D(pool_size=(2, 2), strides=(2,2))(x)
+        x = Conv2D(64, (3,3), activation='relu')(x)
+        x = MaxPool2D(pool_size=(2, 2), strides=(2,2))(x)
+        x = Flatten()(x)
+        x = Dense(128, activation='relu')(x)
+        outputs = Dense(n_classes, activation='softmax')(x)
+        self.model = Model(inputs, outputs)
+        self.model.summary()
+
+    @staticmethod
+    def network_acc(custom_acc):
+        def acc(y_true,y_pred):
+            targ = K.argmax(y_true, axis=-1)
+            pred = K.argmax(y_pred, axis=-1)
+
+            correct = K.cast(K.equal(targ, pred), dtype='float32')
+            Pmax = K.cast(K.max(y_pred, axis=-1), dtype='float32')
+
+            Pmax = Pmax * correct
+            mask = K.cast(K.greater(Pmax, custom_acc), dtype='float32')
+
+            return K.mean(mask)
+        return acc
+
+
+    def train(self):
+        self.model.compile(
+            loss='categorical_crossentropy',
+            optimizer=Adam(learning_rate),
+            metrics=['accuracy']
+            # metrics=[
+            #     TrafficClassifier.network_acc(
+            #                             custom_acc=custom_acc
+            #                                  )]
+        )
+        self.history = self.model.fit(
+                            self.X,
+                            self.Y,
+                            batch_size=batch_size,
+                            epochs=num_epoches,
+                            validation_split=validation_split
+                            )
+
+    def load_model(self):
+        loaded_model = load_model(model_weights)
+        loaded_model.compile(
+                        loss='categorical_crossentropy',
+                        optimizer=Adam(learning_rate),
+                        metrics=['accuracy']
+                        # metrics=[
+                        #     TrafficClassifier.network_acc(
+                        #                             custom_acc=custom_acc
+                        #                                 )]
+                        )
+        self.model = loaded_model
+
+    def save_model(self):
+        self.model.save(model_weights)
+
+    def evaluation(self, X):
+        Ypred = self.model.predict(X)
+        Ppred = np.max(Ypred, axis=-1)
+        unk = (Ppred <= custom_acc)
+        Punk = np.mean(unk) * 100
+        return Punk
+
+    def unknown_evaluation(self):
+        print("-----Unknown percentage------")
+        print("Train Data : {}%".format(self.evaluation(self.X)))
+        print("Test Data : {}%".format(self.evaluation(self.Xtest)))
+
+    def predict_classes(self):
+        Ypred = self.model.predict(self.X)
+
+        N = Ypred.shape[0]
+        Ppred = np.argmax(Ypred, axis=-1)
+        Ponehot = np.zeros((N, n_classes), dtype=np.int64)
+        for i in range(N):
+           j = Ppred[i]
+           Ponehot[i,j] = 1
+        Pclasses = self.encoder.inverse_transform(Ponehot).reshape(-1,)
+        class_count = dict(Counter(Pclasses.tolist()))
+        class_count = sorted(class_count.items(),key=operator.itemgetter(1),reverse=True)
+        for label, value in class_count:
+            fraction = (value/N)*100
+            fraction = round(fraction, 3)
+            print("{} : {}%".format(label,fraction))
+
+    def bin_probability(self,Confidence):
+        bins = np.linspace(0, 1, n_bins)
+        digitized = np.digitize(Confidence, bins)
+        return digitized
+
+    def plot_histogram(self, x1, x2):
+        Wx1 = np.empty(x1.shape)
+        Wx1.fill(1/len(x1))
+        Wx2 = np.empty(x2.shape)
+        Wx2.fill(1/len(x2))
+
+        plt.hist(
+                [x1, x2], 
+                bins = n_bins,
+                weights=[Wx1, Wx2],
+                histtype ='bar',
+                color = colors, 
+                label=names
+                )
+
+        plt.legend()
+        plt.xlabel('confidence')
+        plt.ylabel('Distribution')
+        plt.title('Confidence distribution of Train and Test data')
+        plt.savefig(confidence_img)
+        plt.show()
+
+    def predict_distribution(self):
+        Ptrain = self.model.predict(self.X)
+        Ptest = self.model.predict(self.Xtest)
+
+        ConfTrain = np.max(Ptrain, axis=-1)
+        ConfTest = np.max(Ptest, axis=-1)
+
+        ConfTrain_bin = self.bin_probability(ConfTrain) / n_bins
+        ConfTest_bin = self.bin_probability(ConfTest) / n_bins
+        self.plot_histogram(ConfTrain_bin, ConfTest_bin)
+
+    def run(self):
+        if os.path.exists(model_weights):
+            print("Loading the CNN !!!")
+            self.load_model()
+        else:
+            print("Training the CNN !!!")
+            self.classifier()
+            self.train()
+            # self.save_model()
+        # self.unknown_evaluation()
+        # self.predict_distribution()
+
+if __name__ == "__main__":
+    model = TrafficClassifier()
+    model.run()
